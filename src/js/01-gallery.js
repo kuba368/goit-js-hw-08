264e1cd714890eb22a19018149b12c09d8d2abaa
Gniewomir368 <79970452+Gniewomir368@users.noreply.github.com>
2023-03-31 12:46:54 +0200

    Edited JSON

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,7 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// import '../css/common.css';
-// import '../css/01-gallery.css';
+import '../css/common.css';
+import '../css/01-gallery.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
