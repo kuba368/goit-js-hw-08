1da7acc9dd132200d18a6115d49a8c8c39e5580f
Gniewomir368 <79970452+Gniewomir368@users.noreply.github.com>
2023-03-31 12:31:38 +0200

    Update 1.0

---
+++ src/js/01-gallery.js
@@ -0,0 +1,33 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// import '../css/common.css';
+// import '../css/01-gallery.css';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryElement = document.querySelector('.gallery');
+
+const createGallery = () => {
+  const galleryMarkup = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" />
+</a>`
+    )
+    .join('');
+  galleryElement.insertAdjacentHTML('afterbegin', galleryMarkup);
+};
+createGallery();
+
+const galleryLightBox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+  captionsData: 'alt',
+  enableKeyboard: true,
+});
